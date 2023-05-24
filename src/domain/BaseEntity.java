7efd70717e7d0e933daeafd4db93b9646564b66f
arbadrloo <arbadrloo@gmail.com>
2023-05-25 02:32:41 +0330

    initiate the project & add base entity.

---
+++ src/domain/BaseEntity.java
@@ -0,0 +1,20 @@
+package domain;
+
+public class BaseEntity<T> {
+    private Long id;
+
+    public BaseEntity(Long id) {
+        this.id = id;
+    }
+
+    public BaseEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
